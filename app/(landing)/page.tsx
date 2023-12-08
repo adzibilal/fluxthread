0380586225c5a425d4c3db96ec18c2e2856a06d6
adzibilal <adzibilal02@gmail.com>
2023-12-08 13:00:51 +0700

    change meta & ico

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,11 @@
 import Hero from "./_components/Hero";
 import LandingCollection from "./_components/LandingCollection";
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'FluxThread | Landing Page',
+  description: 'Be bold to be different with FluxThread',
+}
 
 export default function Home() {
   return (
